7c0b969ba81783320557e2a122db7c899b66e82f
daffaakmal09 <sdaffaakmalm@gmail.com>
2023-05-28 14:51:36 +0700

    projek react

--- src/App.jsx
+++ src/App.jsx
@@ -15,18 +15,23 @@ function App() {
 
   const [newTodo, setNewTodos] = useState('')
   const [todos, setTodos] = useState(DUMMY_TODOS)
+  const [error, setError] = useState('')
 
   function addNewTodo(){
-    const updatedTodo = [...todos]
-    const objTodo = {
-      id: nanoid(),
-      title: newTodo,
-      isCompleted:false
+    if (newTodo.length === 0 ) {
+      setError('data kosong')
+    }else{
+      const updatedTodo = [...todos]
+      const objTodo = {
+        id: nanoid(),
+        title: newTodo,
+        isCompleted:false
+      }
+      updatedTodo.push(objTodo)
+      setTodos(updatedTodo)
+      setNewTodos([])
     }
     // console.log(objTodo)
-    updatedTodo.push(objTodo)
-    setTodos(updatedTodo)
-    setNewTodos([])
   }
 
   function completeTodo(targetTodo){
@@ -41,17 +46,31 @@ function App() {
     console.log(updatedTodos)
   }
 
+  function handleChange(event) {
+    setNewTodos(event.target.value)
+    setError('')
+  }
+
   return (
     <>
     <h1>Coding seru</h1>
-    <input type='text' placeholder='Isi' value={newTodo} onChange={event => (setNewTodos(event.target.value))} />
+    <input type='text' placeholder='Isi' value={newTodo} onChange={event => (handleChange(event))} />
+    
     <button onClick={() => addNewTodo()}>Create</button>
+    {
+      error.length > 0 ? (
+        <p style={{color: 'red'}}>{error}</p>
+      ) : null
+    }
     <ul>
       {
         todos.map((todo) => (
           <li key={todo.id} className='todo-item' style={{textDecoration: todo.isCompleted ? 'line-through' : 'none'}}> 
-            <input type='checkbox' onChange={() => completeTodo(todo.id)}/> 
-            {todo.title}
+            <p>
+              <input type='checkbox' onChange={() => completeTodo(todo.id)}/> 
+              {todo.title}
+            
+            <button style={{ marginLeft: '16px', backgroundColor: 'red'}}>x</button></p>
           </li>
         ))
       }
