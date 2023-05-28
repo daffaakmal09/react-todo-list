8a4cc70f60e01e7c960f838478ac98328ecd610a
daffaakmal09 <sdaffaakmalm@gmail.com>
2023-05-28 13:28:11 +0700

    add create todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,46 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { nanoid } from 'nanoid'
 import './App.css'
+
+const DUMMY_TODOS = [
+  {
+    i:nanoid(),
+    title:'Belajar React',
+    isCompleted:false
+  }
+]
 
 function App() {
   // const [count, setCount] = useState(0)
 
+  const [newTodo, setNewTodos] = useState('')
+  const [todos, setTodos] = useState(DUMMY_TODOS)
+
+  function addNewTodo(){
+    const updatedTodo = [...todos]
+    const objTodo = {
+      id: nanoid(),
+      title: newTodo,
+      isCompleted:false
+    }
+    console.log(objTodo)
+    updatedTodo.push(objTodo)
+    setTodos(updatedTodo)
+    setNewTodos([])
+  }
+
   return (
     <>
     <h1>Coding seru</h1>
-    
+    <input type='text' placeholder='Isi' value={newTodo} onChange={event => (setNewTodos(event.target.value))} />
+    <button onClick={() => addNewTodo()}>Create</button>
+    <ul>
+      {
+        todos.map((todo) => (
+          <li key={todo.id}> <input type='checkbox' /> {todo.title}</li>
+        ))
+      }
+    </ul>
 
     </>
   )
